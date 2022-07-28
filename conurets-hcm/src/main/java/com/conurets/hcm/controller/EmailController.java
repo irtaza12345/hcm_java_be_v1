35381c54195238cb08bcfe66fc29170aa4e0a602
AdeelArif <adeel.arif@conurets.com>
2022-07-28 18:33:36 +0500

    Configuration changes from application.properties to application.yml

--- conurets-hcm/src/main/java/com/conurets/hcm/controller/EmailController.java
+++ conurets-hcm/src/main/java/com/conurets/hcm/controller/EmailController.java
@@ -1,17 +1,22 @@
 package com.conurets.hcm.controller;
 
+import com.conurets.hcm.base.dto.response.BaseResponseDTO;
 import com.conurets.hcm.commons.utils.EmailUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping(value = "/api/email/")
+//@RequestMapping(value = "/api/email/")
 public class EmailController {
 
-    @GetMapping(value = "testEmail")
-    public String TestEmail(){
+    @ResponseBody
+    @GetMapping(value = "/api/email/testEmail")
+    public BaseResponseDTO<?> TestEmail(){
 
-        return EmailUtils.TestEmail("Hassan Adnan");
+        BaseResponseDTO <String> baseResponseDTO = new BaseResponseDTO<>();
+        baseResponseDTO.setData(EmailUtils.TestEmail("Hassan Adnan"));
+        return baseResponseDTO;
     }
 }
